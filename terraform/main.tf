474e18b1f3c573b42759a4979238570082cc4ec6
bhupender <gaurb8863@gmail.com>
2026-05-21 21:57:49 +0530

    complete assignment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,19 @@
 provider "aws" {
+  access_key = "test"
+  secret_key = "test"
+
   region = var.region
 
   endpoints {
     ec2 = "http://localhost:4566"      # LocalStack endpoint for EC2
     s3  = "http://localhost:4566"      # LocalStack endpoint for S3
   }
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
 }
 
 locals {
