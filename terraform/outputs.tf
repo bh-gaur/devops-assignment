8eb82194d796ba74f52d33b22bc507eb288348c0
bhupender <gaurb8863@gmail.com>
2026-05-22 13:04:49 +0530

    complete assignment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,10 +3,18 @@ output "vpc_id" {
 }
 
 output "subnet_ids" {
-  value = module.network.public_subnet_ids
+  value = [
+
+    module.network.public_subnet_ids[0],
+    module.network.public_subnet_ids[1],
+  ]
 }
 
 output "bucket_name" {
   value = aws_s3_bucket.aws_s3.id
+}
+
+output "security_group_id" {
+  value = module.network.security_group_id
 }
 
